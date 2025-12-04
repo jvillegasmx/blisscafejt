cfd37139d67fda27247569a01b3f8a362a26b73c
Javier Villegas <javier@theoctocode.com>
2025-12-03 20:17:22 -0600

    Added menus

--- public/index.php
+++ public/index.php
@@ -1,14 +1,32 @@
 <?php
-// Router script for PHP built-in server to mimic .htaccess behavior
+// Secure router script
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// If requesting a path without extension, try to serve the .html file
-if (!pathinfo($requestUri, PATHINFO_EXTENSION)) {
-    $htmlFile = __DIR__ . $requestUri . '.html';
-    if (file_exists($htmlFile)) {
+// Serve index.html at root
+if ($requestUri === '/' || $requestUri === '') {
+    if (file_exists(__DIR__ . '/index.html')) {
         header('Content-Type: text/html; charset=UTF-8');
-        readfile($htmlFile);
+        readfile(__DIR__ . '/index.html');
+        exit;
+    }
+}
+
+// Security: Remove any directory traversal attempts
+$requestUri = str_replace(['../', '..\\', "\0"], '', $requestUri);
+$requestUri = ltrim($requestUri, '/');
+
+// If requesting a path without extension, try to serve the .html file
+if (!pathinfo($requestUri, PATHINFO_EXTENSION) && $requestUri !== '') {
+    $htmlFile = __DIR__ . '/' . $requestUri . '.html';
+
+    // Security: Verify the file is within the public directory
+    $realPath = realpath($htmlFile);
+    $publicDir = realpath(__DIR__);
+
+    if ($realPath && strpos($realPath, $publicDir) === 0 && file_exists($realPath)) {
+        header('Content-Type: text/html; charset=UTF-8');
+        readfile($realPath);
         exit;
     }
 }
@@ -16,8 +34,11 @@ if (!pathinfo($requestUri, PATHINFO_EXTENSION)) {
 // If requesting a .html file directly, redirect to version without extension
 if (preg_match('/\.html$/', $requestUri)) {
     $cleanUri = preg_replace('/\.html$/', '', $requestUri);
-    header("Location: $cleanUri", true, 301);
-    exit;
+    // Security: Only allow relative redirects within the site
+    if (strpos($cleanUri, '/') === 0 || strpos($cleanUri, 'http') === false) {
+        header("Location: /$cleanUri", true, 301);
+        exit;
+    }
 }
 
 // Otherwise, let PHP serve the file normally
